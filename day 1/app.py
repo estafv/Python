d913b0775ce3b75be45881544556e341ea37e12b
esta <estabusiness1@gmail.com>
2026-07-13 20:06:56 -0700

    Add files via upload

---
+++ day 1/app.py
@@ -0,0 +1,23 @@
+#print("Hello, World")
+#print('i am iron man')
+#print("testing 1 2 3 ")
+#print("hi helllo hi")
+#Hello computer, you cannot read this only i can heh
+
+print("""I love pizza"
+and I love coding
+      i love food
+      i love to eat""")
+
+print("I am Spider Man. \n" + "No I AM SPIDER MAN. \n" + "Fine i can be iron man. \n")
+
+
+#print("I am Poppy\n" * 100)
+print("Hello" * 10)
+
+print("Fortnite + Minecraft + Roblox = Fun")
+
+
+name = "Ronald"
+print (name + " i am a nice guy!!!!")
+
