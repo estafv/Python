3ed8f44a4074d3f04a46b8b62aca0a5d1439125b
esta <estabusiness1@gmail.com>
2026-07-14 09:15:55 -0700

    Add files via upload

---
+++ day 2/app.py
@@ -0,0 +1,20 @@
+#Let's build a robot Barista!!
+
+print("Hello, Welcome to the Robot Barista!!!!!!!!!")
+
+name = input ("What is your name? ")
+
+print(name)
+
+print("Hello " + name + ", thank you so much for coming into today!!")
+
+menu = "Latte, Mocha, Tea, Cookie"
+
+print("What would you like from our menu today?\n" + menu)
+
+order = input()
+
+print("Thank you for your order of " + order + ", it will be ready shortly!!")
+
+
+
